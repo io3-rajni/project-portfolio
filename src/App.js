b0711336d46f685bd481ceae7e9c2ffc106e4163
Gajni <io3.rajni@gmail.com>
2023-06-09 18:43:02 +0530

    cardData

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Portfolio from "./component/Portfolio";
+import CardData from "./component/CardData";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Portfolio />
+      <CardData />
     </div>
   );
 }
--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,19 +1,21 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import AdbIcon from "@mui/icons-material/Adb";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { CardMedia, Grid, MenuItem } from "@mui/material";
+import { AdbIcon, MenuIcon } from "@mui/icons-material/Adb";
+import {
+  CardMedia,
+  Grid,
+  MenuItem,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Avatar,
+  Button,
+  Tooltip,
+  Card,
+  CardContent,
+} from "@mui/material";
 // import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
