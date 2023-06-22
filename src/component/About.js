33d7151c07bf7c2cb5bdd7a8704a7dca5c8a71b7
Rajni <io3.rajni@gmail.com>
2023-06-22 20:20:30 +0530

    box setup

--- src/component/About.js
+++ src/component/About.js
@@ -25,60 +25,86 @@ const About = () => {
   // console.log("ShowError", showError);
   return (
     <>
-      <Grid item container spacing={1} sx={{ backgroundColor: "black" }}>
-        <Grid item xs={12} sx={{ padding: " 8% 0px 0px 8%" }}>
-          <Box>
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "700", color: "#b5b0b0" }}
-            >
-              GET IN TOUCH
-            </Typography>
+      <Grid
+        item
+        container
+        spacing={1}
+        sx={{ backgroundColor: "black", padding: " 1% 0px 0px 8%" }}
+      >
+        <Box sx={{ width: "60%", backgroundColor: "#292929", marginTop: "5%" }}>
+          <Grid item xs={12}>
+            <Box>
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: "700", color: "#b5b0b0" }}
+              >
+                GET IN TOUCH
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                variant="h4"
+                sx={{
+                  paddingTop: "8px",
+                  fontWeight: "900",
+                  fontSize: "4rem",
+                  color: "#D4D4D8",
+                }}
+              >
+                Contact.
+              </Typography>
+            </Box>
+          </Grid>
+          <Box component="form" sx={{}} noValidate autoComplete="on">
+            <div>
+              <Typography
+                variant="h5"
+                sx={{ color: "#D4D4D8", paddingTop: "5%" }}
+              >
+                Your Name
+              </Typography>
+
+              <TextField
+                id="outlined-password-input"
+                label="Your Name"
+                type="text"
+                autoComplete="name"
+                sx={{ width: " 80%", backgroundColor: "black" }}
+              />
+            </div>
+            <div>
+              <Typography
+                variant="h5"
+                sx={{ color: "#D4D4D8", paddingTop: "5%" }}
+              >
+                Your Email{" "}
+              </Typography>
+
+              <TextField
+                id="outlined-password-input"
+                label="Email"
+                type="email"
+                autoComplete="on"
+                sx={{ width: " 80%", backgroundColor: "black" }}
+              />
+            </div>
+            <div>
+              <Typography
+                variant="h5"
+                sx={{ color: "#D4D4D8", paddingTop: "5%" }}
+              >
+                Message
+              </Typography>
+              <Box sx={{ width: " 80%", backgroundColor: "black" }}>
+                <TextareaAutosize
+                  aria-label="minimum height"
+                  // minRows={30}
+                  // minCol={30}
+                  sx={{ color: "red !important" }}
+                />
+              </Box>
+            </div>
           </Box>
-          <Box>
-            <Typography
-              variant="h4"
-              sx={{
-                paddingTop: "8px",
-                fontWeight: "900",
-                fontSize: "4rem",
-                color: "white",
-              }}
-            >
-              Contact.
-            </Typography>
-          </Box>
-        </Grid>
-        <Box component="form" sx={{}} noValidate autoComplete="off">
-          <div>
-            <Typography variant="h5">Your Name</Typography>
-
-            <TextField
-              id="outlined-password-input"
-              label="Your Name"
-              type="text"
-              autoComplete="name"
-            />
-          </div>
-          <div>
-            <Typography variant="h5">Your </Typography>
-
-            <TextField
-              id="outlined-password-input"
-              label="Email"
-              type="email"
-              autoComplete="on"
-            />
-          </div>
-          <div>
-            <Typography variant="h5">Message</Typography>
-
-            <TextareaAutosize
-              aria-label="minimum height"
-              minRows={3}
-              // placeholder="Minimum 3 rows"
-            ></TextareaAutosize>
-          </div>
         </Box>
       </Grid>
     </>
